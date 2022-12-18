2db1b1e651c9e79608ff9203901ee2b1ed9ce922
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-19 00:48:27 +0900

    Homeコンポーネントを作成しrouteingの設定

--- resources/js/route.js
+++ resources/js/route.js
@@ -6,12 +6,14 @@ import {
     Switch,
   } from 'react-router-dom';
 import Example from './pages/Example';
+import Home from './pages/Home';
 
   function App() {
     return (
         <div>
             <Switch>
                 {/* この中にルーティングを書くことになる */}
+                <Route path='/' exact component={Home} />
                 <Route path='/example' exact component={Example} />
             </Switch>
         </div>
