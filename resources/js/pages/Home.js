c389dc5d85aad0214debd9c3e41e0f1acb6743b5
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-20 08:22:17 +0900

    Gridレイアウト

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,42 +1,52 @@
 import React from 'react';
-import { Button, Card, Box } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
 
-//スタイルの定義
-const useStyles = makeStyles((theme) => createStyles({
-    card: {
-        margin: theme.spacing(5),
-        padding: theme.spacing(3),
-    },
+// import { makeStyles, createStyles } from '@material-ui/core/styles';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
 }));
+
+// //スタイルの定義
+// const useStyles = makeStyles((theme) => createStyles({
+//     card: {
+//         margin: theme.spacing(5),
+//         padding: theme.spacing(3),
+//     },
+
+// }));
 
 function Home() {
     //定義したスタイルを利用するための設定
-    const classes = useStyles();
+    // const classes = useStyles();
 
     return (
-        <div class="container">
-            <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
-                <Button>Save</Button>
-            </Box>
-
-            <Box
-                sx={{
-                    width: 300,
-                    height: 300,
-                    backgroundColor: 'primary.dark',
-                    '&:hover': {
-                        backgroundColor: 'primary.main',
-                        opacity: [0.9, 0.8, 0.7],
-                    },
-                }}
-            />
-        </div>
-
-
-    );
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      );
 }
 
 export default Home;
