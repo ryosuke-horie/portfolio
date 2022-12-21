6ebb884f8b812291936c8940e49862e908b9a8e8
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-21 18:33:41 +0900

    業務経歴部分と個人開発部分を作成

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 // import { styled } from '@mui/material/styles';
-import { Card, CardHeader, Avatar } from "@material-ui/core";
+import { Card, CardHeader, CardContent, Avatar } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
+import Typography from '@mui/material/Typography';
+
 
 function Home() {
   return (
@@ -12,28 +14,56 @@ function Home() {
           <Card>
             <CardHeader
               avatar={<Avatar>P</Avatar>}
-              title="Profile"
-              subheader="プロフィール"
+              title="堀江 遼佑"
+              subheader="バックエンドエンジニア"
             />
           </Card>
         </Grid>
         <Grid item xs={6}>
-          <Card>xs=6</Card>
+          <Card>
+            <CardHeader
+              title="業務経歴1"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                株式会社いえらぶGROUP
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                不動産会社向けSaaSサービス「いえらぶ」の開発。
+                上記サービスと連携した集客用HPサービスの開発。
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
         <Grid item xs={6}>
-          <Card>xs=6</Card>
+        <Card>
+            <CardHeader
+              title="業務経歴2"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                マミヤITソリューション株式会社
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                受託開発に従事  
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item xs={3}>
-          <Card>xs=3</Card>
-        </Grid>
-        <Grid item xs={3}>
-          <Card>xs=3</Card>
-        </Grid>
-        <Grid item xs={3}>
-          <Card>xs=3</Card>
-        </Grid>
-        <Grid item xs={3}>
-          <Card>xs=3</Card>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader
+              title="個人開発"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                格闘技向け減量管理アプリ
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                開発中
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </div>
